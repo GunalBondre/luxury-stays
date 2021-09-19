66d3c376ddc1f29be3462200dcf17ef3aea161a9
ninad <bondre.gunal@gmail.com>
2021-09-19 20:13:42 +0530

    preparing to deploy

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const indexRoute = require("./routes/index");
 const authRouter = require("./routes/authRoute");
 const hotelRouter = require("./routes/hotelRoute");
 const paymentRouter = require("./routes/paymentRoute");
-
+const path = require("path");
 require("./model/db");
 // middlewares
 app.use(cors());
@@ -20,6 +20,13 @@ app.use("/auth", authRouter);
 app.use("/hotel", hotelRouter);
 app.use("/billing", paymentRouter);
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 // port
 app.listen(port, () => {
 	console.log(`server listening on port ${port}`);
